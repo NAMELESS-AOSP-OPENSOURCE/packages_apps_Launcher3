968cd2a4849af0cb046d97805df6e2e43e1e95d8
Joey <joey@lineageos.org>
2018-10-10 08:27:37 +0200

    Launcher3: add toggle for desktop and drawer labels
    Squashed with:
    
    Author: razorloves <razorloves@gmail.com>
    Date:   Wed Jan 8 23:53:25 2020 -0600
    
        Trebuchet: Fix missing icon labels in folders
    
        Fixes regression introduced in 186796e.
    
        Change-Id: I1e6054fe84ea54bf2a85bf6d26760196ffe78f69
    
    Author: LuK1337 <priv.luk@gmail.com>
    Date:   2021-01-13 17:01:22 +0100
    
        Trebuchet: Honor desktop show labels preference for folder icons
    
        Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/2937
    
        Change-Id: I5769057809890cd8086ec01d6e9d1be87fe17c25
    
    Change-Id: I98063b7adaf22029c4bfa50d2cac730f3612e121
    Signed-off-by: Joey <joey@lineageos.org>

--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -92,6 +92,8 @@ public class InvariantDeviceProfile implements SharedPreferences.OnSharedPrefere
     private static final float ICON_SIZE_DEFINED_IN_APP_DP = 48;
 
     public static final String KEY_WORKSPACE_LOCK = "pref_workspace_lock";
+    public static final String KEY_SHOW_DESKTOP_LABELS = "pref_desktop_show_labels";
+    public static final String KEY_SHOW_DRAWER_LABELS = "pref_drawer_show_labels";
 
     // Constants that affects the interpolation curve between statically defined device profile
     // buckets.
@@ -208,10 +210,12 @@ public class InvariantDeviceProfile implements SharedPreferences.OnSharedPrefere
 
     @TargetApi(23)
     private InvariantDeviceProfile(Context context) {
+        SharedPreferences prefs = Utilities.getPrefs(context);
+        prefs.registerOnSharedPreferenceChangeListener(this);
         String gridName = getCurrentGridName(context);
         String newGridName = initGrid(context, gridName);
         if (!newGridName.equals(gridName)) {
-            Utilities.getPrefs(context).edit().putString(KEY_IDP_GRID_NAME, newGridName).apply();
+            prefs.edit().putString(KEY_IDP_GRID_NAME, newGridName).apply();
         }
         new DeviceGridState(this).writeToPrefs(context);
 
@@ -232,6 +236,8 @@ public class InvariantDeviceProfile implements SharedPreferences.OnSharedPrefere
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         if (DeviceProfile.KEY_PHONE_OVERVIEW_GRID.equals(key)) {
             onConfigChanged(mContext, false);
+        } else if (KEY_SHOW_DESKTOP_LABELS.equals(key) || KEY_SHOW_DRAWER_LABELS.equals(key)) {
+            onConfigChanged(mContext);
         }
     }
 
