f42c0b8b690f06fc4c7f46c7f0f0da8afcd3d76d
cjh1249131356 <cjh1249131356@gmail.com>
2022-05-09 20:26:04 +0800

    Launcher3: Add a script to import themed icon
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

---
+++ sync_icon.sh
@@ -0,0 +1,5 @@
+RES=~/nameless/packages/overlays/ThemeIcons/PixelLauncherIconsOverlay/res
+cp -f $RES/drawable/* res/drawable/
+cp -f $RES/values/arrays.xml res/values/custom_arrays.xml
+cp -f $RES/values/dimens.xml res/values/custom_dimens.xml
+cp -f $RES/xml/grayscale_icon_map.xml res/xml/grayscale_icon_map.xml
