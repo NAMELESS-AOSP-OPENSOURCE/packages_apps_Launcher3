553a2f535a5bcbcd06190ca4c650599b608eb9d4
cjh1249131356 <cjh1249131356@gmail.com>
2022-05-17 23:09:19 +0800

    Launcher3: Update themed icons
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>

--- sync_icon.sh
+++ sync_icon.sh
@@ -1,5 +1,4 @@
 RES=~/nameless/packages/overlays/ThemeIcons/PixelLauncherIconsOverlay/res
+rm -rf res/drawable/themed_icon*
 cp -f $RES/drawable/* res/drawable/
-cp -f $RES/values/arrays.xml res/values/custom_arrays.xml
-cp -f $RES/values/dimens.xml res/values/custom_dimens.xml
 cp -f $RES/xml/grayscale_icon_map.xml res/xml/grayscale_icon_map.xml
